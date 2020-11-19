b2a90353f44b21b7efb4baa4e01927ef6aa3fb03
sword20 <72461412+sword20@users.noreply.github.com>
2020-11-19 09:49:29 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
-echo '<p>This is PHP!</p>;
+//echo '<p>This is PHP!</p>;
+
+if(isset($_GET["FirstName"])){//show feedback
+  echo $_GET["FirstName"];
+}else{//show form
+  echo '
+  <form action=''>
+  First Name: <input type="text" name="FirstName" /><br />
+  <input type="submit" />
+  </form>
+  ';
+}
+
+
+
+
 ?>
-<p>This is HTML</p>
+

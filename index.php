43c069f94138fd43e704f7e183241d76ccaf9c7e
sword20 <72461412+sword20@users.noreply.github.com>
2020-11-24 09:49:13 -0800

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,17 @@ if(isset($_GET["FirstName"])){//show feedback
   <form action="">
   <p>First Name: <input type="text" name="FirstName" /></p>
   <p>Last Name: <input type="text" name="LastName" /></p>
+  
+  <p>Favorite Color:</p>
+  <p><input type="radio" name="FavoriteColor" value="red" />Red </p>
+  <p><input type="radio" name="FavoriteColor" value="blue" />Blue </p>
+  <p><input type="radio" name="FavoriteColor" value="yellow" />Yellow </p>
+  
+  <p>Sundae Toppings:</p>
+  <p><input type="radio" name="Toppings[]" value="oreas" />Oreos </p>
+  <p><input type="radio" name="Toppings[]" value="whipped cream" />Whipped Cream </p>
+  <p><input type="radio" name="Toppings[]" value="crushednuts" />Crushed Nuts </p>
+  
   <input type="submit" />
   </form>
   ';
